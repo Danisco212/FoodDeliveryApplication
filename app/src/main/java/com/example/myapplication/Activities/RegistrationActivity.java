0b346845e94aa6f396f8657beb207073e95d7a3a
Danisco212 <58591021+Danisco212@users.noreply.github.com>
2020-09-22 06:44:14 +0700

    User registration fixed

--- app/src/main/java/com/example/myapplication/Activities/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/RegistrationActivity.java
@@ -4,13 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.myapplication.Helpers.CheckUserInput;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -18,6 +25,8 @@ public class RegistrationActivity extends AppCompatActivity {
     private ProgressBar loading;
     private TextView error;
     private Toolbar toolbar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,7 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     private void initialize(){
+        mAuth = FirebaseAuth.getInstance();
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Register");
         setSupportActionBar(toolbar);
@@ -40,6 +50,42 @@ public class RegistrationActivity extends AppCompatActivity {
         password = findViewById(R.id.register_password);
         loading = findViewById(R.id.loading);
         error = findViewById(R.id.errorMessage);
+    }
+
+    public void register(View view){
+        loading.setVisibility(View.VISIBLE);
+        error.setVisibility(View.GONE);
+        CheckUserInput checkUserInput = new CheckUserInput();
+        if (checkUserInput.correctInputs(email, password, name)){
+            if (checkUserInput.validEmail(email)){
+                mAuth.createUserWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString().trim())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(RegistrationActivity.this, MainActivity.class)
+                                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK));
+                                RegistrationActivity.this.finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                showErrorMessage(e.getMessage());
+                            }
+                        });
+            }else{
+                showErrorMessage("Invalid email");
+            }
+        }else{
+            showErrorMessage("Fill all the fields");
+        }
+    }
+
+    // show error message
+    private void showErrorMessage(String errorMsg){
+        loading.setVisibility(View.GONE);
+        error.setText(errorMsg);
+        error.setVisibility(View.VISIBLE);
     }
 
     @Override
